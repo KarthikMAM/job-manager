cb88be4db79e2feefb92977da990bfc22a49621f
Karthik M A M <karthik_m_a_m@outlook.com>
2018-09-13 00:17:09 +0530

    Update Entry Path

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'index.js',
+    file: 'dist/bundle.js',
     format: 'umd',
     name: 'QueueManager',
   },
