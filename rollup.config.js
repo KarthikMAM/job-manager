be1c6d2830b97702a0fe77a152f0762070a432db
Karthik M A M <karthik_m_a_m@outlook.com>
2018-09-13 00:16:39 +0530

    Minify Builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
@@ -9,5 +10,6 @@ export default {
   },
   plugins: [
     babel(),
+    terser(),
   ],
 }
